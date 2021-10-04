7081938164100ebc9eef455a659e9008e8b0b9b9
Christopher Nielsen <c.consult.christopher@gmail.com>
2021-10-04 16:29:38 +0200

    init

---
+++ methane_sensor_v0/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 10/4/2021 4:27:30 PM
+ *  Author: Mainframe
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
